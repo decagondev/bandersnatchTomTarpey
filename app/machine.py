943d6abb796e3fb34236a15fb0f67ae8cfb3b602
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-06 06:51:31 +0000

    document Machine class

--- app/machine.py
+++ app/machine.py
@@ -1,4 +1,16 @@
 class Machine:
+    """A customized Random Forest Classifier tailored for working with our dataset.
+
+        Attributes:
+        -----------
+        name : str
+            A descriptor for the classifier, defaulted to "Random Forest Classifier".
+        model : RandomForestClassifier
+            The trained and tuned Random Forest model.
+        timestamp : str
+            The date and time when the model was trained.
+
+    """
 
     def __init__(self, df):
         pass
