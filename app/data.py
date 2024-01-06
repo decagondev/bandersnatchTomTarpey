5a0e63f1fa5b059eb2017a0e7c9cb1297f8b30d6
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-06 06:34:51 +0000

    step1 added docs to Database class

--- app/data.py
+++ app/data.py
@@ -8,7 +8,30 @@ from pymongo import MongoClient
 
 
 class Database:
+    """ Class to create and manage a Database that stores Monsters with their attributes.
 
+    This class stores and manages Monster data in a MongoDB database. The class provides methods to
+    seed the database with Monsters, reset the database, count the instances of monsters and extract
+    data in different formats.
+
+    Attributes:
+        collection: A collection in a MongoDB database where you want the data stored.
+
+    Methods:
+        - __init__(self, collection: str): Initializes the Database class, with the collection name gives as a string.
+        - seed(self, amount: int): Seeds the collection with specified amount of Monster instances
+        - count(self) -> int: Counts and returns the number of monsters in the collection
+        - dataframe(self) -> DataFrame: Returns data in the collection as a pandas DataFrame
+        - html_table(self) -> str: Returns html table of the data for display
+
+    Example:
+        db = Database('Monsters')
+        db.seed(1000)
+        print(db.count())  # Output : 1000
+        db.reset()
+        print(db.count())  # Output : 0
+
+    """
     def seed(self, amount):
         pass
 
